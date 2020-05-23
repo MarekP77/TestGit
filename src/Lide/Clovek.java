709f8583e301092c06a208cb091eb318aae6ffc5
MarekP77 <posm04@vse.cz>
2020-05-23 17:02:46 +0200

    První commit

---
+++ src/Lide/Clovek.java
@@ -0,0 +1,26 @@
+package Lide;
+
+public class Clovek {
+    public String jmeno = "Karel Nový";
+    private int vek = 25;
+    private int unava = 0;
+
+    public int beh(int km){
+        if (unava >= 0 && unava < 20){
+            unava = unava + km;
+        } else System.out.println("Jsem příliš unavený");
+        return unava;
+    }
+
+    public int spat(int spanek){
+        if (unava >= 10 && unava <= 20){
+            unava = unava - spanek;
+        }
+        return unava;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s (%d)", jmeno, vek);
+    }
+}
---
+++ src/Lide/Lide.java
@@ -0,0 +1,11 @@
+package Lide;
+
+public class Lide {
+    public static void main(String[] args) {
+        Clovek karel = new Clovek();
+        System.out.println(karel);
+        karel.beh(10);
+        karel.beh(10);
+        karel.beh(10);
+    }
+}
